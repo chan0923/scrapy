ccc0cc88c9b7a69c3a5e0a77d09e862c8c38cbcc
Yeehui Chan <yeehchan@ebay.com>
2017-07-10 22:37:35 +0800

    udpate cron

--- iproperty/iproperty/cron.sh
+++ iproperty/iproperty/cron.sh
@@ -3,5 +3,5 @@
 cd /home/yeehchan/dev/scrapy
 source /home/yeehchan/dev/scrapy/venv/bin/activate
 cd /home/yeehchan/dev/scrapy/iproperty/iproperty/
-scrapy crawl iproperty
+scrapy crawl iproperty &> results/log_$(date +\%Y\%m\%d).txt
 
